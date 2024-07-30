5a59dd0ad8311af8c323a0bcd195c0ab44e42689
shobhan17 <bellamkondashobhanbabu@gmail.com>
2024-07-30 14:07:52 +0530

    installed tailwind css

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="text-blue-700">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dignissimos vel aliquam dolorum placeat tempore facere quam repudiandae eius minus adipisci?</div>
+  )
+}
+
+export default App
